e717e156f527f213ce9a26d5f3ecea6d27e67b9f
Christian <Chrisaduenas@gmail.com>
2020-04-15 17:30:20 -0700

    checkpoint

--- welcome/welcome-router.js
+++ welcome/welcome-router.js
@@ -1,10 +1,14 @@
+//this reads in all of environment variables from .env
+// require("dotenv").config()
+
 const express = require("express")
 
 const router = express.Router()
 
 router.get("/", (req, res) => {
 	res.status(200).json({
-		message: "Welcome",
+		message: `Welcome ${process.env.COHORT}`,
+		fact: process.env.FUN_FACT || "I have no fun facts",
 	})
 })
 
